c8ea2eadb1741aec2627a3805a7cd13e4e074dae
jsta <stachel2@msu.edu>
2020-01-22 13:44:09 -0500

    fix file name typo

---
+++ tests/testthat/test-display-names.R
@@ -0,0 +1,13 @@
+context("display_names")
+
+test_that("display_names works", {
+
+  skip_on_cran()
+  skip_on_travis()
+  skip_on_appveyor()
+
+  lg <- lagosus_load("locus")
+
+  expect_equal(display_names(names(lg$locus$locus_information))[22],
+               "ID (hu4)")
+})
