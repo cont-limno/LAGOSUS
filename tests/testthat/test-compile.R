59a69f35fe8a2d541e2bdc7cbad96865c6d5d48a
jsta <stachel2@msu.edu>
2020-12-15 16:54:38 -0500

    towards full windows compile testing

--- tests/testthat/test-compile.R
+++ tests/testthat/test-compile.R
@@ -21,14 +21,14 @@ test_that("locus compile works on windows", {
 
   lagosus_compile(
     locus_version = "1.0",
-    locus_folder = "~/Downloads/LAGOSUS_LOCUS/LOCUS_v1.0",
+    locus_folder = "../../../../../Downloads/LOCUS_v1.0",
     locus_overwrite = TRUE,
     dest_folder = ".")
 
   expect_true(file.exists("locus_1.0.qs"))
 })
 
-test_that("limno compile works", {
+test_that("limno compile works on linux", {
   # Only test lagosus_compile manually via: `test(filter = "compile")`
   skip_on_ci()
   skip_on_cran()
@@ -37,6 +37,21 @@ test_that("limno compile works", {
   lagosus_compile(
     limno_version = "2.1",
     limno_folder = "~/Downloads/LAGOSUS_LIMNO/US/LIMNO_v2.1/Final exports",
+    limno_overwrite = TRUE,
+    dest_folder = ".")
+
+  expect_true(file.exists("limno_2.1.qs"))
+})
+
+test_that("limno compile works on windows", {
+  # Only test lagosus_compile manually via: `test(filter = "compile")`
+  skip_on_ci()
+  skip_on_cran()
+  skip_if(.Platform$OS.type == "unix")
+
+  lagosus_compile(
+    limno_version = "2.1",
+    limno_folder = "../../../../../Downloads/LIMNO_v2.1",
     limno_overwrite = TRUE,
     dest_folder = ".")
 
