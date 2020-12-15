53a68ea88d4fe62f6628041b2d51b536a86fb7ff
jsta <stachel2@msu.edu>
2020-12-15 16:48:03 -0500

    start windows compile tests

--- tests/testthat/test-compile.R
+++ tests/testthat/test-compile.R
@@ -1,7 +1,23 @@
-test_that("locus compile works", {
+test_that("locus compile works on linux", {
   # Only test lagosus_compile manually via: `test(filter = "compile")`
   skip_on_ci()
   skip_on_cran()
+  skip_if(.Platform$OS.type == "windows")
+
+  lagosus_compile(
+    locus_version = "1.0",
+    locus_folder = "~/Downloads/LAGOSUS_LOCUS/LOCUS_v1.0",
+    locus_overwrite = TRUE,
+    dest_folder = ".")
+
+  expect_true(file.exists("locus_1.0.qs"))
+})
+
+test_that("locus compile works on windows", {
+  # Only test lagosus_compile manually via: `test(filter = "compile")`
+  skip_on_ci()
+  skip_on_cran()
+  skip_if(.Platform$OS.type == "unix")
 
   lagosus_compile(
     locus_version = "1.0",
@@ -16,6 +32,7 @@ test_that("limno compile works", {
   # Only test lagosus_compile manually via: `test(filter = "compile")`
   skip_on_ci()
   skip_on_cran()
+  skip_if(.Platform$OS.type == "windows")
 
   lagosus_compile(
     limno_version = "2.1",
